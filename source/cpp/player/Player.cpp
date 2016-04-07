cb0b06d0773f37fa5ff4eafc0ab70750d80bd323
yu-chan <y.ooki0125@outlook.com>
2016-04-07 15:08:24 +0900

    構造体の追加

---
+++ source/cpp/player/Player.cpp
@@ -0,0 +1,11 @@
+#include "..\..\h\player\Player.h"
+
+
+Player::Player()
+{
+}
+
+
+Player::~Player()
+{
+}
---
+++ source/h/struct.h
@@ -0,0 +1,27 @@
+//プレイヤー、敵、オブジェクトに使う
+typedef struct {
+	float x, y, z;//座標
+	float ang;//角度
+	int flag, count;//フラグ、出現時間
+	float sp;//スピード
+	float ax, ay, az;//加速度
+	float vx, vy, vz;//速度
+}object;
+
+//キャラクターがもつステータス
+typedef struct {
+	object status;
+	int hp, hp_mx;
+}character;
+
+//弾
+typedef struct {
+	object status;
+	int power, num;
+}bullet;
+
+//プレイヤー
+typedef struct {
+	character player;
+	bullet shot;
+};
