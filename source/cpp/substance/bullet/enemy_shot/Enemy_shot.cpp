7f4b50088ecee9c413cd1e2676ca19efc3c6bfcc
yu-chan <y.ooki0125@outlook.com>
2016-05-12 16:44:22 +0900

    Enemyの発車時間がcountと同じなら、syotを打てるようにした

--- source/cpp/substance/bullet/enemy_shot/Enemy_shot.cpp
+++ source/cpp/substance/bullet/enemy_shot/Enemy_shot.cpp
@@ -14,17 +14,22 @@ Enemy_shot::~Enemy_shot()
 
 void Enemy_shot::shot_regist() {
 	substance *sub = enemy.getSub();
-	if (sub[0].count == 30) {
-		substance *_sub = getSub();
-		_sub[0].x = sub[0].x;
-		_sub[0].y = sub[0].y;
-		_sub[0].z = sub[0].z;
-		_sub[0].vz = -0.1f;
-		_sub[0].flag = true;
-		//ショット音のフラグを立てる
-		music.se_flag(SHOT_NO);
+	substance *_sub = getSub();
+	for (int i = 0; i < getSize(); i++) {
+		//敵が出現してから増えた時間がショットを打つ時間と同じになったら、弾を打つ
+		if (sub[i].count == 30) {
+			//DrawFormatString(0, 0, GetColor(255, 255, 255), "count:%d", _sub[i].count);
+			_sub[i].x = sub[i].x;
+			_sub[i].y = sub[i].y;
+			_sub[i].z = sub[i].z;
+			_sub[i].vz = -0.1f;
+			_sub[i].count = 0;
+			_sub[i].flag = true;
+			//ショット音のフラグを立てる
+			music.se_flag(SHOT_NO);
+		}
+		music.se_play(SHOT_NO);
 	}
-	music.se_play(SHOT_NO);
 }
 
 //bool Enemy_shot::collision_detection() {
@@ -33,18 +38,32 @@ void Enemy_shot::shot_regist() {
 
 void Enemy_shot::move() {
 	substance *sub = getSub();
-	sub[0].z += sub[0].vz;
-	sub[0].count++;
+	for (int i = 0; i < getSize(); i++) {
+		if (sub[i].flag == true) {
+			sub[i].z += sub[i].vz;
+			sub[i].count++;
+			DrawFormatString(0, 0, GetColor(255, 255, 255), "count:%d", sub[i].count);
 
-	//相手に当たってるなら、フラグを消す
-	if (collision_detection(&player, VGet(sub[0].x, sub[0].y + 10, sub[0].z), VGet(sub[0].x, sub[0].y + 10, sub[0].z + 5), ModelAirCraft, PLAYER_MODEL_FRAMEINDEX)) {
-		sub[0].flag = false;
-	}
+			//相手に当たってるなら、フラグを消す
+			if (collision_detection(&player, VGet(sub[i].x, sub[i].y + 10, sub[i].z), VGet(sub[i].x, sub[i].y + 10, sub[i].z + 5), ModelAirCraft, PLAYER_MODEL_FRAMEINDEX)) {
+				sub[i].flag = false;
+			}
 
-	//範囲外に出たらフラグを消す
-	if (sub[0].x < SHOT_RANGE_X_MIN || sub[0].x > SHOT_RANGE_X_MAX ||
-		sub[0].y < SHOT_RANGE_Y_MIN || sub[0].y > SHOT_RANGE_Y_MAX ||
-		sub[0].z < SHOT_RANGE_Z_MIN || sub[0].z > SHOT_RANGE_Z_MAX) {
-		sub[0].flag = false;
+			//範囲外に出たらフラグを消す
+			/*
+			弾が出ているか確認しているため、範囲を狭くしている
+			あとに範囲を変える必要がある
+			*/
+			/*if (sub[i].x < SHOT_RANGE_X_MIN || sub[i].x > SHOT_RANGE_X_MAX ||
+				sub[i].y < SHOT_RANGE_Y_MIN || sub[i].y > SHOT_RANGE_Y_MAX ||
+				sub[i].z < SHOT_RANGE_Z_MIN || sub[i].z > SHOT_RANGE_Z_MAX) {
+				sub[i].flag = false;
+			}*/	 
+			if (sub[i].x < SHOT_RANGE_X_MIN || sub[i].x > SHOT_RANGE_X_MAX ||
+				sub[i].y < SHOT_RANGE_Y_MIN || sub[i].y > SHOT_RANGE_Y_MAX ||
+				sub[i].z < SHOT_RANGE_Z_MIN) {
+				sub[i].flag = false;
+			}
+		}
 	}
 }
--- source/cpp/substance/character/enemy/Enemy.cpp
+++ source/cpp/substance/character/enemy/Enemy.cpp
@@ -5,11 +5,11 @@ Enemy::Enemy()
 {
 	mallocSub(1);
 	substance *sub = getSub();
-	sub[0].x = 0;
-	sub[0].y = 0;
-	sub[0].z = 20;
-	sub[0].vx = 0.1f;
-	sub[0].count = 0;
+	//sub[0].x = 0;
+	//sub[0].y = 0;
+	//sub[0].z = 20;
+	//sub[0].vx = 0.1f;
+	//sub[0].count = 0;
 	//sub[0].flag = true;
 }
 
@@ -22,19 +22,27 @@ Enemy::~Enemy()
 void Enemy::move() {
 	substance *sub = getSub();
 	////character &cha = *getCha();
-	if (sub[0].x < ENEMY_RANGE_X_MIN || sub[0].x > ENEMY_RANGE_X_MAX) {
-		sub[0].vx *= -1;
+	for (int i = 0; i < getSize(); i++) {
+		if (sub[i].flag == true) {
+			if (sub[i].x < ENEMY_RANGE_X_MIN || sub[i].x > ENEMY_RANGE_X_MAX) {
+				sub[i].vx *= -1;
+			}
+			sub[i].x += sub[i].vx;
+			sub[i].count++;
+			//DrawFormatString(0, 0, GetColor(255, 255, 255), "count:%d", sub[i].count);
+		}
 	}
-	sub[0].x += sub[0].vx;
-	sub[0].count++;
 }
 
 void Enemy::enemy_regist() {   
 	substance *sub = getSub();
 
 	for (int i = 0; i < getSize(); i++) {
+		//敵の出現時間とステージのカウントが同じなら、敵を出現させる
 		if (sub[i].aptime == stage_count) {
 			sub[i].flag = true;
+			sub[i].vx = 0.1f;
+			sub[i].count = 0;
 		}
 	}
 }
