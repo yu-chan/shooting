660b92b21bd066bb33916538657649a742bb03f0
yu-chan <y.ooki0125@outlook.com>
2016-05-30 21:27:20 +0900

    敵をプレイヤーの方へ移動できるようにした

--- source/cpp/substance/character/enemy/Enemy.cpp
+++ source/cpp/substance/character/enemy/Enemy.cpp
@@ -21,14 +21,32 @@ Enemy::~Enemy()
 
 void Enemy::move() {
 	substance *sub = getSub();
+	substance *sub_ = player.getSub();
 	////character &cha = *getCha();
 	for (int i = 0; i < getSize(); i++) {
 		if (sub[i].flag == true) {
-			if (sub[i].x < ENEMY_RANGE_X_MIN || sub[i].x > ENEMY_RANGE_X_MAX) {
+			//動く前の敵、プレイヤーの座標を保存
+			VECTOR pre_enemy = VGet(sub[i].x, sub[i].y, sub[i].z);
+			VECTOR pre_player = VGet(sub_[0].x, sub_[0].y, sub_[0].z);
+
+			//プレイヤーと敵のベクトルの差
+			VECTOR p_e_sub = VSub(pre_player, pre_enemy);
+
+			//ベクトルのサイズを求める
+			float vsize = VSize(p_e_sub);
+
+			//敵がプレイヤーの方への移動量を1にする
+			p_e_sub = VScale(p_e_sub, 1 / (10 * vsize));
+
+			sub[i].x += p_e_sub.x;
+			sub[i].y += p_e_sub.y;
+			sub[i].z += p_e_sub.z;
+
+			/*if (sub[i].x < ENEMY_RANGE_X_MIN || sub[i].x > ENEMY_RANGE_X_MAX) {
 				sub[i].vx *= -1;
 			}
 			sub[i].x += sub[i].vx;
-			sub[i].count++;
+			sub[i].count++;*/
 			//DrawFormatString(0, 0, GetColor(255, 255, 255), "count:%d", sub[i].count);
 		}
 	}
